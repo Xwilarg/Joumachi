09c3ad2440df4f7bcef5e0c87926e5f4ea667f1f
Xwilarg <xwilarg@yahoo.fr>
2020-12-04 20:14:07 +0100

    Add typo fix

--- Joumachi/Program.cs
+++ Joumachi/Program.cs
@@ -4,15 +4,19 @@ using Discord.WebSocket;
 using DiscordUtils;
 using Joumachi.Module;
 using Newtonsoft.Json;
+using NTextCat;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
+using WeCantSpell.Hunspell;
 
 namespace Joumachi
 {
     class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
                   => new Program().MainAsync().GetAwaiter().GetResult();
 
         public static DiscordSocketClient Client { private set; get; }
@@ -20,6 +24,15 @@ namespace Joumachi
 
         public static Random Rand = new Random();
         public static DateTime StartTime { private set; get; }
+
+        private RankedLanguageIdentifier _identifier;
+
+        private Dictionary<string, WordList> _dictionaries = new Dictionary<string, WordList>();
+
+        private char[] _splitChar = new[]
+        {
+            '.', ' ', '/', '\\', '!', '?', ',', ';', '(', ')', '\n', ':', '_', '`', '*', '"'
+        };
 
         private Program()
         {
@@ -37,6 +50,16 @@ namespace Joumachi
             if (credentials.BotToken == null)
                 throw new NullReferenceException("Invalid Credentials file");
 
+            var factory = new RankedLanguageIdentifierFactory();
+            _identifier = factory.Load("Keys/Core14.profile.xml");
+
+            foreach (var file in Directory.GetFiles("Dictionaries"))
+            {
+                FileInfo fi = new FileInfo(file);
+                if (fi.Name.Split('.')[1] == "dic")
+                    _dictionaries.Add(fi.Name.Split('.')[0], WordList.CreateFromFiles(file));
+            }
+
             await _commands.AddModuleAsync<Communication>(null);
 
             Client.MessageReceived += HandleCommandAsync;
@@ -51,7 +74,7 @@ namespace Joumachi
         private async Task HandleCommandAsync(SocketMessage arg)
         {
             SocketUserMessage msg = arg as SocketUserMessage;
-            if (msg == null) return;
+            if (msg == null || msg.Author.Id == Client.CurrentUser.Id) return;
             int pos = 0;
             if (!arg.Author.IsBot && (msg.HasMentionPrefix(Client.CurrentUser, ref pos) || msg.HasStringPrefix("j.", ref pos)))
             {
@@ -60,6 +83,29 @@ namespace Joumachi
                 if (!result.IsSuccess)
                 {
                     Console.WriteLine(result.Error.ToString() + ": " + result.ErrorReason);
+                }
+                else
+                    return;
+            }
+            var languages = _identifier.Identify(msg.Content);
+            var mostCertainLanguage = languages.FirstOrDefault();
+            if (mostCertainLanguage != null && File.Exists("Dictionaries/" + mostCertainLanguage.Item1.Iso639_2T + ".dic"))
+            {
+                var checker = _dictionaries[mostCertainLanguage.Item1.Iso639_2T];
+                foreach (string s in msg.Content.Split(_splitChar, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    if (!s.Any(x => char.IsLetter(x)))
+                        continue;
+                    if (!checker.Check(s))
+                    {
+                        var word = s.Trim(_splitChar);
+                        var suggestions = checker.Suggest(word).ToArray();
+                        if (suggestions.Length == 0)
+                            await msg.Channel.SendMessageAsync("\"" + s + "\" doesn't exists");
+                        else
+                            await msg.Channel.SendMessageAsync("\"" + s + "\" doesn't exists, maybe you meant \"" + suggestions[0] + "\"?");
+                        break;
+                    }
                 }
             }
         }
